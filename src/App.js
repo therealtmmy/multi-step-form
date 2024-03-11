f6bd80b3dfd335e97761da339c54f76f62b5577b
therealtmmy <oluwatimilehinsamson@gmail.com>
2024-03-11 13:32:02 +0100

    Thank you page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Info from "./Components/NavContent/Info";
+import ThankYou from "./Components/NavContent/ThankYou";
 
 function App() {
   return (
     <div className="App">
       <div className="App-content">
         <Sidebar />
-        <Info />
+        {/* <Info /> */}
+        <ThankYou />
       </div>
     </div>
   );
