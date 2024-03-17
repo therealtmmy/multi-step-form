02637008a72aeda1e2ef8c7b4491ff70d6afc8e1
therealtmmy <oluwatimilehinsamson@gmail.com>
2024-03-17 17:16:13 +0100

    Adding Functionality

--- src/Components/NavContent/Info.jsx
+++ src/Components/NavContent/Info.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Info.css";
 
 const Info = () => {
+  const [info, setInfo] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const Next = () => {
+    console.log(`${info}`);
+  };
+
   return (
     <div className="Info">
       <h1>Personal info</h1>
@@ -9,16 +19,31 @@ const Info = () => {
 
       <form action="" method="post" className="formDiv">
         <label htmlFor="Name">Name</label>
-        <input type="text" placeholder="e.g Stephen King" />
+        <input
+          type="text"
+          value={info.name}
+          onChange={(e) => setInfo({ ...info, name: e.target.value })}
+          placeholder="e.g Stephen King"
+        />
 
         <label htmlFor="Email Address">Email Address</label>
-        <input type="email" placeholder="e.g stephenking@lorem.com" />
+        <input
+          type="email"
+          value={info.email}
+          onChange={(e) => setInfo({ ...info, email: e.target.email })}
+          placeholder="e.g stephenking@lorem.com"
+        />
 
         <label htmlFor="Phone Number">Phone Number</label>
-        <input type="text" placeholder="e.g +1 234 567 890" />
+        <input
+          type="text"
+          value={info.phoneNumber}
+          onChange={(e) => setInfo({ ...info, phoneNumber: e.target.value })}
+          placeholder="e.g +1 234 567 890"
+        />
       </form>
 
-      <button>Next Step</button>
+      <button onClick={Next}>Next Step</button>
     </div>
   );
 };
